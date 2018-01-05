799a3fea6facb7ff27132ab7a87aa92de6efdf94
dzz <devon.zachary@gmail.com>
2018-01-05 00:42:57 -0800

    blendmode rename

---
+++ src/cpp_module/cpp_api/hwgfx/cpp_blendmode.h
@@ -0,0 +1,18 @@
+#ifndef BGLPP_API_HWGFX_BLEND_H
+#define BGLPP_API_HWGFX_BLEND_H
+
+#include <stack>
+#include <functional>
+#include "../../../hwgfx/blend_control.h"
+
+namespace bgl {
+    class blendmode {
+        public:
+            static void use(unsigned int mode, std::function<void()> rendering_function);
+        private:
+            static std::stack<unsigned int> stack;
+    };
+}
+
+#endif
+
